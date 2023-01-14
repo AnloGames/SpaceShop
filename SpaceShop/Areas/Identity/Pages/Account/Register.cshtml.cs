c8cc7ca24c0c0ed0619f028a20d828b7d88e2d9a
AnloGames <7383an@gmail.com>
2023-01-14 20:59:57 +0300

    Cart_01

--- SpaceShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SpaceShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using SpaceShop.Models;
+using SpaceShop;
 
 namespace SpaceShop.Areas.Identity.Pages.Account
 {
@@ -31,15 +32,16 @@ namespace SpaceShop.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
-        //добавляем новую роль
+        // добавляем новую роль
         private readonly RoleManager<IdentityRole> _roleManager;
+
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            RoleManager<IdentityRole> roleManager,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -47,6 +49,7 @@ namespace SpaceShop.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+
             _roleManager = roleManager;
         }
 
@@ -103,16 +106,19 @@ namespace SpaceShop.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
-            public string FullName { get; set; }    
+            public string FullName { get; set; }
         }
 
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!await _roleManager.RoleExistsAsync(PathManager.CustomerRole))
+            // существует ли роль а бд?
+            if (!await _roleManager.RoleExistsAsync(PathManager.AdminRole))
             {
-
+                await _roleManager.CreateAsync(new IdentityRole(PathManager.AdminRole));
+                await _roleManager.CreateAsync(new IdentityRole(PathManager.CustomerRole));
             }
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
@@ -123,10 +129,12 @@ namespace SpaceShop.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { 
-                    FullName = Input.FullName,
+                //var user = CreateUser(); 
+                var user = new ApplicationUser()
+                {
                     UserName = Input.Email,
-                    Email = Input.Email
+                    Email = Input.Email,
+                    FullName = Input.FullName
                 };
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
@@ -137,12 +145,15 @@ namespace SpaceShop.Areas.Identity.Pages.Account
                 {
                     if (User.IsInRole(PathManager.AdminRole))
                     {
+                        // назначение роли
                         await _userManager.AddToRoleAsync(user, PathManager.AdminRole);
                     }
                     else
                     {
                         await _userManager.AddToRoleAsync(user, PathManager.CustomerRole);
                     }
+
+
 
                     _logger.LogInformation("User created a new account with password.");
 
---
+++ SpaceShop/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpaceShop.Data;
+using SpaceShop.Models;
+using SpaceShop.Utility;
+using SpaceShop.ViewModels;
+using System.Diagnostics;
+
+namespace SpaceShop.Controllers
+{
+    public class CartController : Controller
+    {
+        public ApplicationDbContext database;
+
+        public CartController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
+        public IActionResult Index()
+        {
+            List<Cart> cartList = new List<Cart>();
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).ToList();
+            }
+
+            List<int> productIdList = cartList.Select(x => x.ProductId).ToList();
+
+            IEnumerable<Product> productList = database.Product.Where(x => productIdList.Contains(x.Id));
+
+            return View(productList);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<Cart> cartList = new List<Cart>();
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).ToList();
+            }
+            var cart = cartList.Single(x => x.ProductId == id);
+            cartList.Remove(cart);
+            HttpContext.Session.Set(PathManager.SessionCart, cartList);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
